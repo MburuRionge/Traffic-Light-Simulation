3b31380c8d51f70188462b28a81a7faabbf84d28
MburuRionge <victormburu3@gmail.com>
2024-10-19 17:29:35 +0300

    writing the code for traffic light simulation

--- TrafficLightSimulation.ino
+++ TrafficLightSimulation.ino
@@ -1,12 +1,16 @@
 // LED Traffic Light Simulation
 // This program simulates a traffic light system with a pedestrian crossing button.
 
-const int redLightPin = 23;   // Red LED connected to GPIO 23
-const int yellowLightPin = 22; // Yellow LED connected to GPIO 22
+const int redLightPin = 12;   // Red LED connected to GPIO 23
+const int yellowLightPin = 14; // Yellow LED connected to GPIO 22
 const int greenLightPin = 21;  // Green LED connected to GPIO 21
-const int buttonPin = 19;      // Pedestrian crossing button connected to GPIO 19
+const int buttonPin = 1;      // Pedestrian crossing button connected to GPIO 19
 
 void setup() {
+   pinMode(redLightPin, OUTPUT);
+   pinMode(yellowLightPin, OUTPUT);
+   pinMode(greenLightPin, OUTPUT);
+   pinMode(buttonPin, INPUT_PULLUP);  // Use INPUT_PULLUP for button
   // Initialize the LED pins as outputs
   // TODO: Set the button pin as INPUT
 }
@@ -17,5 +21,23 @@ void loop() {
   
   // TODO: Implement pedestrian crossing logic
   // Change light sequence when button is pressed
+  if (digitalRead(buttonPin) == LOW) {  // Button is pressed
+    digitalWrite(redLightPin, LOW);
+    digitalWrite(yellowLightPin, LOW);
+    digitalWrite(greenLightPin, LOW);
+  } else {  // Button is not pressed
+    digitalWrite(redLightPin, HIGH);    // turn on LED1 
+    delay(200);                  // wait for 200ms
+    digitalWrite(yellowLightPin, HIGH);    // turn on LED2
+    delay(200);                  // wait for 200ms       
+    digitalWrite(greenLightPin, HIGH);    // turn on LED3 
+    delay(200);                  // wait for 200ms
+    digitalWrite(redLightPin, LOW);     // turn off LED1
+    delay(300);                  // wait for 300ms
+    digitalWrite(yellowLightPin, LOW);     // turn off LED2
+    delay(300);                  // wait for 300ms
+    digitalWrite(greenLightPin, LOW);     // turn off LED3
+    delay(300);                  // wait for 300ms before running the sequence again
+  }
 }
 
